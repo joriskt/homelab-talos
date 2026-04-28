266f6ccdf0e7374a64d24369d6b9fc79ef40931f
Joris Klein Tijssink <noreply@joris.me>
2026-04-28 16:22:20 +0200

    Update test cluster om Technitium cluster te gebruiken

---
+++ test/source-me.sh
@@ -0,0 +1,4 @@
+#!/bin/.bash
+export CONTROL_PLANE_IP=("192.168.11.51" "192.168.11.52" "192.168.11.53")
+export WORKER_IP=("192.168.11.61" "192.168.11.62" "192.168.11.63")
+export TALOSCONFIG=talosconfig.yaml
